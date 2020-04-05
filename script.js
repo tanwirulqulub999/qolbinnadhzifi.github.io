ad490b866aa9fe21db48f3c97995fdadb0fb3326
MicroClone <43690001+qolbinnadhzifi@users.noreply.github.com>
2020-04-05 13:32:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-//
+// build
 function foo(){
-
+  const data = ["DedSecTL","Cvar1984","CiKu370","ITermSec","etc."];
+  data.push("MicroClone");
+  // output : DedSecTL, Cvar1984, CiKu370, ITermSec, etc., MicroClone
+  data.forEach(xoxo => {
+    console.log(xoxo);
+  });
 }
